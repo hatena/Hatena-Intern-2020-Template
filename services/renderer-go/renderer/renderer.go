44de911a91cbcb90e9d1fd0ec3eb505153f4d216
Nakano Masaki <namachan10777@gmail.com>
2020-08-25 12:01:54 +0900

    avoid to convert markdown's url syntax.

--- services/renderer-go/renderer/renderer.go
+++ services/renderer-go/renderer/renderer.go
@@ -7,19 +7,22 @@ import (
 	"regexp"
 )
 
-var urlRE = regexp.MustCompile(`https?://[^\s]+`)
+var standAloneUrlRE = regexp.MustCompile(`[^(\(.*\)\[)]https?://[^\s]+[^\]]`)
+var exactUrlRE = regexp.MustCompile(`https?://[^\s]+`)
 var linkTmpl = template.Must(template.New("link").Parse(`<a href="{{.}}">{{.}}</a>`))
 
 // Render は受け取った文書を HTML に変換する
 func Render(ctx context.Context, src string) (string, error) {
 	// TODO: これはサンプル実装 (URL の自動リンク) です
-	html := urlRE.ReplaceAllStringFunc(src, func(url string) string {
-		var w bytes.Buffer
-		err := linkTmpl.ExecuteTemplate(&w, "link", url)
-		if err != nil {
-			return url
-		}
-		return w.String()
+	html := standAloneUrlRE.ReplaceAllStringFunc(src, func(standAloneUrl string) string {
+		return exactUrlRE.ReplaceAllStringFunc(standAloneUrl, func(exactUrl string) string {
+			var w bytes.Buffer
+			err := linkTmpl.ExecuteTemplate(&w, "link", exactUrl)
+			if err != nil {
+				return exactUrl
+			}
+			return w.String()
+		})
 	})
 	return html, nil
 }
